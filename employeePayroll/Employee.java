d77994f235d73dc8c13dccbadfa525d2c81be0e2
Nompilo <nompilorwmalinga@gmail.com>
2025-02-16 22:52:12 +0200

    adding exception handling

--- employeePayroll/Employee.java
+++ employeePayroll/Employee.java
@@ -3,39 +3,13 @@ public abstract class Employee {
     private String name;
     private int id;
 
-  
-    public Employee(String name, int id){
-      
+    public Employee(String name, int id) {
+
         this.name = name;
-        this.id=id;
-
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
-      
-    public void setName(String name) {
-        if(!name.isEmpty()){
-            this.name = name;
-        }else{
-            System.out.println("Name cannot be empty");
-        }
-    }
-
-    public void setId(int id) {
-       if(id > 0){
         this.id = id;
-       }else{
-        System.out.println("ID cannot be negative");
-       }
+
     }
 
-    
     public abstract double calculateSalary();
 
     public void displayEmployeeInfo() {
@@ -43,6 +17,5 @@ public abstract class Employee {
         System.out.println("Employee ID: " + id);
         System.out.println("Salary: " + calculateSalary());
     }
-
 
 }
--- employeePayroll/Main.java
+++ employeePayroll/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -8,48 +9,71 @@ public class Main {
         PayrollSystem pSystem = new PayrollSystem();
 
         while (true) {
-            System.out.println("\nChoose Employee Type:");
-            System.out.println("1. Full-Time Employee");
-            System.out.println("2. Part-Time Employee");
-            System.out.println("3. Exit");
-            int choice = scanner.nextInt();
-            scanner.nextLine();
+            try {
+                System.out.println("\nChoose Employee Type:");
+                System.out.println("1. Full-Time Employee");
+                System.out.println("2. Part-Time Employee");
+                System.out.println("3. Exit");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
 
-            if (choice == 3) {
-                System.out.println("Exiting Payroll System...");
-                break;
+                if (choice == 3) {
+                    System.out.println("Exiting Payroll System...");
+                    break;
+                }
+
+                
+                String employeeName;
+                while (true) {
+                    System.out.println("Enter Employee Name: ");
+                    employeeName = scanner.nextLine();
+                    if (isNameValid(employeeName)) {
+                        break;
+                        
+                    }else{
+                        System.out.println("Name should consist of letters only,Please enter a valid name.");
+                    }
+
+                    
+                }
+
+                System.out.println("Enter Employee ID: ");
+                int id = scanner.nextInt();
+
+                Employee employee;
+                if (choice == 1) {
+                    System.out.println("Enter Salary: ");
+                    double salary = scanner.nextDouble();
+                    employee = new FullTimeEmployee(employeeName, id, salary);
+                } else if (choice == 2) {
+                    System.out.println("Enter Hourly Rate: ");
+                    double hourlyRate = scanner.nextDouble();
+
+                    System.out.println("Enter Hours Worked: ");
+                    int hoursWorked = scanner.nextInt();
+
+                    employee = new PartTimeEmployee(employeeName, id, hourlyRate, hoursWorked);
+                } else {
+                    System.out.println("Invalid choice! Please try again.");
+                    continue;
+                }
+
+                pSystem.addEmployee(employee);
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input! Please enter a valid number.");
+                scanner.nextLine();
+
             }
-
-            System.out.println("Enter Employee Name: ");
-            String employeeName = scanner.nextLine();
-
-            System.out.println("Enter Employee ID: ");
-            int id = scanner.nextInt();
-
-            Employee employee;
-            if (choice == 1) {
-                System.out.println("Enter Salary: ");
-                double salary = scanner.nextDouble();
-                employee = new FullTimeEmployee(employeeName, id, salary);
-            } else if (choice == 2) {
-                System.out.println("Enter Hourly Rate: ");
-                double hourlyRate = scanner.nextDouble();
-
-                System.out.println("Enter Hours Worked: ");
-                int hoursWorked = scanner.nextInt();
-
-                employee = new PartTimeEmployee(employeeName, id, hourlyRate, hoursWorked);
-            } else {
-                System.out.println("Invalid choice! Please try again.");
-                continue;
-            }
-
-            pSystem.addEmployee(employee);
         }
 
         System.out.println("\nProcessing Payroll...");
         pSystem.processPayroll();
         scanner.close();
 
+    }
+
+
+    public static boolean isNameValid(String name){
+        return name.matches("[a-zA-Z ]+");
     }
 }
